2cd25a7394f363a38c38875b1625fd4ae12e9d46
zrc <zrc@itcast.cn>
2018-09-03 21:27:18 +0800

    git——test提交

---
+++ git_test_project/src/com/itcast/Hello.java
@@ -0,0 +1,10 @@
+package com.itcast;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+        System.out.println("hello world");
+        System.out.println("hello word");
+        System.out.println("hello world");
+    }
+}
